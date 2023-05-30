e9d50d222c28398f0dc5963400ad5436c3817f2a
WelsonAA <georgeezwelson@outlook.com>
2023-05-30 23:25:49 +0300

    V2.3

--- src/main/java/com/example/GUI/HelloApplication.java
+++ src/main/java/com/example/GUI/HelloApplication.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("bills.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("TransferScene.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setScene(scene);
         stage.show();
